319ad7fec4fd73776871005dbc2ada3f731cb61c
Adam Krone <krone.adam@gmail.com>
2014-06-25 10:31:44 -0500

    Adds name to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name              "exiftool"
 maintainer        "Carlo Zottmann"
 maintainer_email  "carlo@zottmann.org"
 license           "WTFPL"
 description       "Installs exiftool from source"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.mdown'))
-version           "1.1"
+version           "1.1.1"
 supports          "ubuntu"
 
 %w( apt build-essential ).each do |d|
